31abdd3f8bdd3ad34d64818537bc4bef24d1002f
Agnes Anosike <agnesanosike@gmail.com>
2026-05-10 20:24:53 +0100

    fix cors for netlify frontend

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [process.env.FRONTEND_URL, 'http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
